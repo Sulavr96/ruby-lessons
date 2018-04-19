973f4cfc69d3a4820e0aa227d64da3323f825170
sulavr <cyberraid@gmail.com>
2018-04-19 18:16:28 +0545

    practice

--- untitled/helper.rb
+++ untitled/helper.rb
@@ -1,6 +1,10 @@
 class Helper
   array=["ram","ra","hari"]
   array1=["hari","gopal","ram"]
+  array3=[1,2,3,4,5,6]
+  number=3
+
+  sum=0
 if  array.values_at(0)==array.values_at(1)
 puts 'hello'
 
@@ -8,4 +12,15 @@ else
   puts 'tai'
 
 end
-end
+  array3.each do |number|
+    sum=sum+number
+  end
+   avg=sum/array3.size.to_f
+
+ if avg!=number
+   puts "hey"
+ else
+   puts"no"
+ end
+
+  end
--- untitled/test.rb
+++ untitled/test.rb
@@ -1,39 +1,101 @@
 class Test
-
-  diamondArray=[1,2,2,3,5,4,4]
-  sorted_DiamondArray=diamondArray.sort
+  diamond_array=[1,2,3,4,5]
+  sorted_diamond_array=diamond_array.sort
   sum=0
-  person_Count=0
+  person_count=0
   total_person=3
-  sorted_DiamondArray.each do |number|
+  diamond_array_size=sorted_diamond_array.size.to_f
+  diamond_sum=0
+  array_counter=sorted_diamond_array.size
+  sorted_diamond_array.each do |number|
     sum=sum+number
   end
 
   if sum%total_person!=0
-  puts "The Array Cannot be divided"
+    puts "The Array Cannot be divided"
+
+#else if sum/diamond_array_size!=total_person
+ # puts "The Array Cannot be divided"
   else
-    sorted_DiamondArray.each do|number|
-      sorted_DiamondArray.each do|num|
-        a=number
-        b=num
-        if a+b==2*sorted_DiamondArray.last
-          puts "person #{total_person}: #{sorted_DiamondArray.last}"
-          end
-        if a+b==sorted_DiamondArray.last && a<b
-          distributed_arr=Array.new
-          distributed_arr.push(a,b)
 
-          person_Count+=1
-          puts "person #{person_Count}:"+ distributed_arr.to_s
-          end
-        end
+       max_individual_diamond=sum/total_person
+       max_individual_diamond
+    sorted_diamond_array.each do|number|
+        first_diamond=number
+        if first_diamond==max_individual_diamond
+          puts first_diamond
+          sorted_diamond_array.delete(number)
+          array_counter-=1
 
-
-        end
-
+end
     end
+       array_initial_index=0
+
+          for array_index in 0..array_counter-1
+
+            next_diamond=sorted_diamond_array[array_initial_index].to_i+sorted_diamond_array[array_index+1].to_i
+            if next_diamond==max_individual_diamond
+              puts [sorted_diamond_array[array_initial_index],sorted_diamond_array[array_index+1]].to_s
+              sorted_diamond_array.delete(sorted_diamond_array[array_initial_index])
+              sorted_diamond_array.delete(sorted_diamond_array[array_index+1])
+              array_counter-=2
+
+
 
 
 end
+
+
+
+            end
+          end
+
+             if next_diamond>max_individual_diamond
+                puts"The Array Cannot be furthur divided"
+end
+
+       if next_diamond<max_individual_diamond
+                  furthur_diamond=sorted_diamond_array[array_index].to_i+sorted_diamond_array[array_index+1].to_i+sorted_diamond_array[array_index+1+1].to_i
+                      if furthur_diamond==max_individual_diamond
+                        puts [sorted_diamond_array[array_index],sorted_diamond_array[array_index+1],sorted_diamond_array[array_index+2]].to_s
+                        sorted_diamond_array.delete(sorted_diamond_array[array_index])
+                        sorted_diamond_array.delete(sorted_diamond_array[array_index+1])
+                        sorted_diamond_array.delete(sorted_diamond_array[array_index+2])
+
+                        array_counter-=3
+                      end
+                        if furthur_diamond>max_individual_diamond
+                          puts"The Array Cannot be furthur divided"
+                        end
+                      end
+                end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
