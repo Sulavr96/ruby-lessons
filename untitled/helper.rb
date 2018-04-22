c35d2785514727f9361d37563707fdbbe8cbbcad
sulavr <cyberraid@gmail.com>
2018-04-22 12:49:06 +0545

    fixed

--- untitled/helper.rb
+++ untitled/helper.rb
@@ -1,26 +1,2 @@
-class Helper
-  array=["ram","ra","hari"]
-  array1=["hari","gopal","ram"]
-  array3=[1,2,3,4,5,6]
-  number=3
-
-  sum=0
-if  array.values_at(0)==array.values_at(1)
-puts 'hello'
-
-else
-  puts 'tai'
-
-end
-  array3.each do |number|
-    sum=sum+number
-  end
-   avg=sum/array3.size.to_f
-
- if avg!=number
-   puts "hey"
- else
-   puts"no"
- end
-
-  end
+a=[1,2,3,4,5]
+puts a.first
--- untitled/test.rb
+++ untitled/test.rb
@@ -1,91 +1,47 @@
 class Test
-  diamond_array=[1,2,3,4,5]
-  sorted_diamond_array=diamond_array.sort
-  sum=0
-  person_count=0
-  total_person=3
-  diamond_array_size=sorted_diamond_array.size.to_f
-  diamond_sum=0
-  array_counter=sorted_diamond_array.size
-  sorted_diamond_array.each do |number|
-    sum=sum+number
-  end
+  diamond_array = [1, 1, 2, 2, 2, 4]
+  sorted_diamond_array = diamond_array.sort
+  people = []
+  total_person = 3
+  sum = sorted_diamond_array.sum
 
-  if sum%total_person!=0
+  if sum % total_person != 0
     puts "The Array Cannot be divided"
-
-#else if sum/diamond_array_size!=total_person
- # puts "The Array Cannot be divided"
   else
 
-       max_individual_diamond=sum/total_person
-       max_individual_diamond
-    sorted_diamond_array.each do|number|
-        first_diamond=number
-        if first_diamond==max_individual_diamond
-          puts first_diamond
-          sorted_diamond_array.delete(number)
-          array_counter-=1
+    hand_capacity = 1
+    max_individual_diamond = sum / total_person
+    max_individual_diamond
+    diamonds_in_hand = []
+    while (hand_capacity < sorted_diamond_array.length) do
 
-end
-    end
-       array_initial_index=0
+      sorted_diamond_array.each do |number|
+        diamonds_in_hand.push(number)
+        sorted_diamond_array[diamonds_in_hand.length...sorted_diamond_array.length].each_with_index do |picked_diamond, index|
+          if (diamonds_in_hand.sum == max_individual_diamond)
+            people.push(diamonds_in_hand)
+            #   remove diamonds in hand from sorted array
 
-          for array_index in 0..array_counter-1
-
-            next_diamond=sorted_diamond_array[array_initial_index].to_i+sorted_diamond_array[array_index+1].to_i
-            if next_diamond==max_individual_diamond
-              puts [sorted_diamond_array[array_initial_index],sorted_diamond_array[array_index+1]].to_s
-              sorted_diamond_array.delete(sorted_diamond_array[array_initial_index])
-              sorted_diamond_array.delete(sorted_diamond_array[array_index+1])
-              array_counter-=2
-
-
-
-
-end
-
-
-
+          else
+            diamonds_in_hand.push(picked_diamond)
+            if (diamonds_in_hand.sum < max_individual_diamond)
+              diamonds_in_hand.pop(picked_diamond)
+            else
+              #   cannot divide case
             end
           end
 
-             if next_diamond>max_individual_diamond
-                puts"The Array Cannot be furthur divided"
+          if (index == sorted_diamond_array.length - 1)
+            hand_capacity += 1
+          end
+
+        end
+
+      end
+    end
+
+  end
 end
-
-       if next_diamond<max_individual_diamond
-                  furthur_diamond=sorted_diamond_array[array_index].to_i+sorted_diamond_array[array_index+1].to_i+sorted_diamond_array[array_index+1+1].to_i
-                      if furthur_diamond==max_individual_diamond
-                        puts [sorted_diamond_array[array_index],sorted_diamond_array[array_index+1],sorted_diamond_array[array_index+2]].to_s
-                        sorted_diamond_array.delete(sorted_diamond_array[array_index])
-                        sorted_diamond_array.delete(sorted_diamond_array[array_index+1])
-                        sorted_diamond_array.delete(sorted_diamond_array[array_index+2])
-
-                        array_counter-=3
-                      end
-                        if furthur_diamond>max_individual_diamond
-                          puts"The Array Cannot be furthur divided"
-                        end
-                      end
-                end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
